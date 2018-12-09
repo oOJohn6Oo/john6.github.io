d4ca70504b7ce642bc6f2ca6d4b0a20288c19600
qiang.liu <qiang.liu@definesys.com>
2018-12-09 11:50:54 +0800

    innit

---
+++ js/home.js
@@ -0,0 +1,72 @@
+﻿var isClicked = false;
+var isShowed = false;
+
+function demo1(){
+	var up = document.getElementById("container1");
+	var upbag = document.getElementById("demo1");
+	var down=document.getElementById("container2");
+	var downbag=document.getElementById("index");
+	upbag.style.visibility="visible";
+	up.style.webkitTransform="translateZ(0)";
+	down.style.webkitTransform="translateZ(-200px)";
+	
+	upbag.style.webkitTransform="translateY(0)";
+	up.style.webkitTransform="rotateX(0)";
+	down.style.webkitTransform="rotateX(-90deg)";
+	downbag.style.webkitTransform="translateY(100%)";
+	//response.setHeader("refresh","3;url=www.baidu.com"); 
+	//setTimeout("location.href='http://www.baidu.com'",1200);	
+}
+function transform(){
+	if(isClicked){
+		document.getElementById('main').style.transform='translateX(0) rotateY(0deg)';
+		document.getElementById('nav').style.transform='translateX(-120px)';
+	}
+	else{
+		document.getElementById('main').style.transform='translateX(120px) rotateY(30deg)';
+		document.getElementById('nav').style.transform='translateX(0)';
+	}
+	isClicked=!isClicked;
+}
+
+function show(){
+	if(isShowed){
+		document.getElementById("head").style.filter='blur(0px)';
+		document.getElementById("main").style.filter='blur(0px)';
+		document.getElementById("nav").style.filter='blur(0px)';
+		//document.getElementById("ov-btmleft").style.visibility='hidden';
+		//video.parentNode.removeChild(video);
+		document.getElementById("ov-btmleft").style.display='none';
+	}
+	else{
+		document.getElementById("head").style.filter='blur(10px)';
+		document.getElementById("main").style.filter='blur(10px)';
+		document.getElementById("nav").style.filter='blur(10px)';
+		//document.getElementById("ov-btmleft").style.visibility='visible';
+		//video.parentNode.addChild(video);
+		document.getElementById("ov-btmleft").style.display='inline';
+	}
+	isShowed=!isShowed;
+}
+
+window.onload=function(){
+    function $(id){
+      return document.getElementById(id);
+    }
+//鼠标进
+    $("arr").onmouseover=function(){
+		if(!isClicked){
+			$("main").style.transform='translateX(10px) rotateY(2deg)';
+			$("nav").style.transform='translateX(-110px)';
+		}
+    }
+//鼠标出
+    $("arr").onmouseout=function(){
+		if(!isClicked){
+			$("main").style.transform='translateX(0) rotateY(0)';
+			$("nav").style.transform='translateX(-120px)';
+		}
+    }
+	var ht=$("main").offsetHeight-100;
+	$("main").style.height=ht+'px';
+}
---
+++ js/home_beta.js
@@ -0,0 +1,73 @@
+﻿var isClicked = false;
+var isShowed = false;
+
+function demo1(){
+	var up = document.getElementById("container1");
+	//var upbag = document.getElementById("demo1");
+	var down=document.getElementById("main");
+	$('#main').rotate3Di('flip',3000);
+}
+
+function transform(){
+	if(isClicked){
+		document.getElementById('main').style.transform='perspective(2000px) translateX(0) rotateY(0deg)';
+		document.getElementById('nav').style.transform='translateX(-120px)';
+	}
+	else{
+		document.getElementById('main').style.transform='perspective(2000px) translateX(120px) rotateY(30deg)';
+		document.getElementById('nav').style.transform='translateX(0)';
+	}
+	isClicked=!isClicked;
+}
+
+function show(id){
+	if(isShowed){
+		document.getElementById("main").style.filter='blur(0px)';
+		document.getElementById("nav").style.filter='blur(0px)';
+		document.getElementById("ov-btmleft").style.display='none';
+		$('.overlay>:first-child').attr('src','#');
+	}
+	else{
+		document.getElementById("main").style.filter='blur(10px)';
+		document.getElementById("nav").style.filter='blur(10px)';
+		document.getElementById("ov-btmleft").style.display='inline';
+		if(id=="banner1")
+			$('.overlay>:first-child').attr('src','http://player.youku.com/embed/XOTE2NTkwNzI4');
+		else if(id=="banner2")
+			$('.overlay>:first-child').attr('src','http://player.youku.com/embed/XNzI5MTIwMjk2');
+		else if(id=="banner3")
+			$('.overlay>:first-child').attr('src','https://www.nowcoder.com/ta/coding-interviews?page=1');
+		else
+			$('.overlay>:first-child').attr('src','http://player.youku.com/embed/XMzYxMzEwMTM4OA==');
+	}
+	isShowed=!isShowed;
+}
+
+$('document').ready(function(){
+		$('.nav_btn').hover(function(){
+			if(!isClicked){
+				$("#main").css('transform','perspective(2000px) translateX(10px) rotateY(2deg)');
+				$("#nav").css('transform','translateX(-110px)');
+			}},function(){
+			if(!isClicked){
+				$("#main").css('transform','perspective(2000px) translateX(0) rotateY(0)');
+				$("#nav").css('transform','translateX(-120px)');
+			}
+		});
+		$(".nav_btn").click(function(){
+			if(!isClicked){
+				$("#main").css('transform','perspective(2000px) translateX(120px) rotateY(15deg)');
+				$("#nav").css('transform','translateX(0)');
+			}
+			else{
+				$("#main").css('transform','perspective(2000px) translateX(0) rotateY(0)');
+				$("#nav").css('transform','translateX(-120px)');
+			}
+			isClicked=!isClicked;
+		});
+		$(".c_box").click(function(){
+			show($(this).attr('id'));
+		});
+	}
+	);
+	
